b5579dd38645d4438fa510dfb4ea4ef4fe886c56
Justin Breneman <justin.breneman@yahoo.com>
2020-07-02 23:04:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
-doc = Nokogiri::HTML(html)
-courses = doc.css("id-3c239880-48cf-5ce7-84d0-578546d87586 inlineMobileLeft-2Yo002 imageTextBlockGrid2-3jXtmC")
-
-
 def course
   html = open("https://flatironschool.com/")
   doc = Nokogiri::HTML(html)
